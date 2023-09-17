86344f531adea81c5d19540dfd2ad1f5c294badc
AliGhaffarian <alighaffarian9@gmail.com>
2023-09-17 22:09:22 +0330

    added method get_key

--- src/ui/utilities.rs
+++ src/ui/utilities.rs
@@ -1,9 +1,14 @@
-
+use std::io;
 pub struct util{}
 impl util
 {
     pub fn is_all_digits(input: & String) -> bool {
         let trimmed_input = input.trim();
         trimmed_input.chars().all(|c| c.is_digit(10))
+    }
+    pub fn get_key() {
+        println!("press Enter to continue");
+        let mut input= "".to_string();
+        let _ = io::stdin().read_line(&mut input).unwrap();
     }
 }
